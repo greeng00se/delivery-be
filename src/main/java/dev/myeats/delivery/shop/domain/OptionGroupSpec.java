2abe5f8775c9c7dd41862abc44834d27fc058a1e
greeng00se <thegreengoose7@gmail.com>
2022-08-30 17:46:34 +0900

    [#6] feat: 옵션그룹 클래스 추가

---
+++ src/main/java/dev/myeats/delivery/shop/domain/OptionGroupSpec.java
@@ -0,0 +1,44 @@
+package dev.myeats.delivery.shop.domain;
+
+import dev.myeats.delivery.common.time.BaseTimeEntity;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class OptionGroupSpec extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "OPTION_GROUP_SPEC_ID")
+    private Long id;
+
+    private String name;
+    private boolean exclusive;
+    private boolean basic;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "OPTION_GROUP_SPEC_ID")
+    private List<OptionSpec> optionSpecs = new ArrayList<>();
+
+    @Builder
+    public OptionGroupSpec(String name, boolean exclusive, boolean basic, List<OptionSpec> optionSpecs) {
+        this.name = name;
+        this.exclusive = exclusive;
+        this.basic = basic;
+        this.optionSpecs = optionSpecs;
+    }
+}
