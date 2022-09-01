285d92478547e952372fb9747585a2f8f6d23caa
greeng00se <thegreengoose7@gmail.com>
2022-09-01 20:19:01 +0900

    [#1] feat: 401, 404 응답 반환을 위한 클래스 추가

---
+++ src/main/java/dev/myeats/delivery/common/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package dev.myeats.delivery.common.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 유효한 자격증명 없이 접근시 401 Unauthorized 반환을 위한 엔드포인트
+ */
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
