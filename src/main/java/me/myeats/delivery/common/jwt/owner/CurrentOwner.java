7d95d556f81cd53f8bb977c1f2f2e174166759ce
greeng00se <thegreengoose7@gmail.com>
2022-09-10 23:53:43 +0900

    [#16] feat: owner에 대한 AuthenticationPrincipal 애너테이션 추가

---
+++ src/main/java/me/myeats/delivery/common/jwt/owner/CurrentOwner.java
@@ -0,0 +1,14 @@
+package me.myeats.delivery.common.jwt.owner;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "owner")
+public @interface CurrentOwner {
+}
