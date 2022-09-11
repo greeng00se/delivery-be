5243099517156019e472664be0d83f9efcfe9938
greeng00se <thegreengoose7@gmail.com>
2022-09-11 21:23:34 +0900

    [#16] test: 인증을 위해 CustomSecurityContext 클래스 생성
    - @CurrentOwner를 사용하는 메서드 테스트 할 때 사용

---
+++ src/test/java/me/myeats/delivery/test/CustomSecurityContextFactory.java
@@ -0,0 +1,26 @@
+package me.myeats.delivery.test;
+
+import lombok.RequiredArgsConstructor;
+import me.myeats.delivery.common.jwt.owner.OwnerUserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+@RequiredArgsConstructor
+public class CustomSecurityContextFactory implements WithSecurityContextFactory<WithCustomOwner> {
+
+    private final OwnerUserDetailsService ownerUserDetailsService;
+
+    @Override
+    public SecurityContext createSecurityContext(WithCustomOwner owner) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        User user = ownerUserDetailsService.loadUserByUsername(owner.username());
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+        context.setAuthentication(auth);
+        return context;
+    }
+}
