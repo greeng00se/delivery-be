40aa7453be4b08a7b7317715f697a604f28484df
greeng00se <thegreengoose7@gmail.com>
2022-09-21 01:20:39 +0900

    [#31] feat: List<ShopDto> 반환을 위한 메서드 추가

--- src/main/java/me/myeats/delivery/shop/domain/ShopRepository.java
+++ src/main/java/me/myeats/delivery/shop/domain/ShopRepository.java
@@ -1,6 +1,14 @@
 package me.myeats.delivery.shop.domain;
 
+import me.myeats.delivery.shop.dto.ShopDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    @Query("select new me.myeats.delivery.shop.dto.ShopDto(s.name, s.open, s.minOrderAmount, s.address, s.phoneNumber) " +
+            "from Shop s where s.ownerId = :ownerId")
+    List<ShopDto> findShopDtoListByOwnerId(Long ownerId);
 }
