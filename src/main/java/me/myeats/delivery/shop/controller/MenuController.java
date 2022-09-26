a737b77efc0da721d46558454198c838e964ee18
greeng00se <thegreengoose7@gmail.com>
2022-09-27 00:25:23 +0900

    [#41] feat: 메뉴 저장 기능 추가

--- src/main/java/me/myeats/delivery/shop/controller/MenuController.java
+++ src/main/java/me/myeats/delivery/shop/controller/MenuController.java
@@ -2,6 +2,8 @@ package me.myeats.delivery.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.myeats.delivery.common.jwt.owner.CurrentOwner;
+import me.myeats.delivery.owner.domain.Owner;
 import me.myeats.delivery.shop.dto.MenuSaveRequestDto;
 import me.myeats.delivery.shop.service.MenuService;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,18 +12,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/shop/{id}/menu")
+@RequestMapping("/shop/{shopId}/menu")
 public class MenuController {
 
     private final MenuService menuService;
 
     @PostMapping
-    public Long save(@PathVariable Long id,
-                     @RequestBody MenuSaveRequestDto menuSaveRequestDto) {
+    public void save(@PathVariable Long shopId,
+                     @Valid @RequestBody MenuSaveRequestDto menuSaveRequestDto,
+                     @CurrentOwner Owner owner) {
         log.info(menuSaveRequestDto.toString());
-        return menuService.save(id, menuSaveRequestDto);
+        menuService.save(shopId, menuSaveRequestDto, owner.getId());
     }
 }
--- src/main/java/me/myeats/delivery/shop/dto/MenuDto.java
+++ src/main/java/me/myeats/delivery/shop/dto/MenuDto.java
@@ -1,26 +1,21 @@
 package me.myeats.delivery.shop.dto;
 
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Getter
-@ToString
+@Data
 @NoArgsConstructor
 public class MenuDto {
 
-    @NotBlank
     private String name;
-    @NotBlank
+
     private String description;
-    @NotNull
+
     private Long price;
-    @NotNull
+    
     private Long priority;
 
     private List<OptionGroupSpecDto> optionGroups;
