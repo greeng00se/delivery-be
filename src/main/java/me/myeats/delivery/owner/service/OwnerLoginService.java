c8224b2a7db859a2678dd34e7550b68139c689c3
greeng00se <thegreengoose7@gmail.com>
2022-09-28 22:42:59 +0900

    [#45] feat: UnauthorizedException 추가
    - 인증 실패시 사용

--- src/main/java/me/myeats/delivery/owner/service/OwnerLoginService.java
+++ src/main/java/me/myeats/delivery/owner/service/OwnerLoginService.java
@@ -1,8 +1,10 @@
 package me.myeats.delivery.owner.service;
 
 import lombok.RequiredArgsConstructor;
+import me.myeats.delivery.common.exception.authentication.UnauthorizedException;
 import me.myeats.delivery.common.jwt.TokenProvider;
 import me.myeats.delivery.owner.dto.OwnerLoginDto;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -20,8 +22,12 @@ public class OwnerLoginService {
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(loginDto.getName(), loginDto.getPassword());
 
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return tokenProvider.createToken(authentication);
+        try {
+            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return tokenProvider.createToken(authentication);
+        } catch (BadCredentialsException e) {
+            throw new UnauthorizedException();
+        }
     }
 }
