298ef529965a09e9c35733b261fa86ea8dd2f981
greeng00se <thegreengoose7@gmail.com>
2022-10-10 20:57:45 +0900

    [#60] feat: 고객 엔티티 추가

---
+++ src/main/java/me/myeats/delivery/customer/domain/Customer.java
@@ -0,0 +1,49 @@
+package me.myeats.delivery.customer.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.myeats.delivery.common.jwt.AuthRole;
+import me.myeats.delivery.common.time.BaseTimeEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Customer extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "CUSTOMERs_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "AUTHORITIES")
+    @Enumerated(EnumType.STRING)
+    private AuthRole authority = AuthRole.ROLE_CUSTOMER;
+
+    @Builder
+    public Customer(String name, String email, String password, AuthRole authority) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.authority = authority;
+    }
+}
