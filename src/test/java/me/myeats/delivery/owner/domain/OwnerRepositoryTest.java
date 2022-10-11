20184de24e2beda34dffb49b2e512dd4b2029650
greeng00se <thegreengoose7@gmail.com>
2022-10-11 21:17:37 +0900

    [#60] test: OwnerRepositoryTest existsByName 테스트 추가

--- src/test/java/me/myeats/delivery/owner/domain/OwnerRepositoryTest.java
+++ src/test/java/me/myeats/delivery/owner/domain/OwnerRepositoryTest.java
@@ -23,7 +23,7 @@ class OwnerRepositoryTest {
     @Test
     @DisplayName("이름을 통한 단일 조회")
     void findOneByName() {
-        // givens
+        // given
         Owner owner = OwnerFixtures.owner().build();
         ownerRepository.save(owner);
 
@@ -32,5 +32,25 @@ class OwnerRepositoryTest {
 
         // then
         assertThat(findOwner).isEqualTo(owner);
+    }
+
+    @Test
+    @DisplayName("이름이 없는 경우 True 반환")
+    void existsByNameReturnTrue() {
+        // given
+        Owner owner = OwnerFixtures.owner()
+                .name("HELLO")
+                .build();
+        ownerRepository.save(owner);
+
+        // except
+        assertThat(ownerRepository.existsByName("HELLO")).isTrue();
+    }
+
+    @Test
+    @DisplayName("이름이 없는 경우 False 반환")
+    void existsByNameReturnFalse() {
+        // except
+        assertThat(ownerRepository.existsByName("NOTHING")).isFalse();
     }
 }
