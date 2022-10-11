28d2579ab53a7281614d2c3bfdd55550bbff89a7
greeng00se <thegreengoose7@gmail.com>
2022-10-11 18:41:53 +0900

    [#60] fix: LoginService 수정

--- src/main/java/me/myeats/delivery/owner/service/OwnerLoginService.java
+++ src/main/java/me/myeats/delivery/owner/service/OwnerLoginService.java
@@ -1,16 +1,18 @@
 package me.myeats.delivery.owner.service;
 
 import lombok.RequiredArgsConstructor;
-import me.myeats.delivery.common.exception.authentication.UnauthorizedException;
 import me.myeats.delivery.common.jwt.TokenProvider;
 import me.myeats.delivery.owner.dto.request.OwnerLoginRequestDto;
 import me.myeats.delivery.owner.dto.response.OwnerLoginResponseDto;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static me.myeats.delivery.common.jwt.AuthRole.ROLE_OWNER;
 
 @RequiredArgsConstructor
 @Service
@@ -20,15 +22,13 @@ public class OwnerLoginService {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     public OwnerLoginResponseDto login(OwnerLoginRequestDto loginDto) {
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginDto.getName(), loginDto.getPassword());
-
-        try {
-            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            return new OwnerLoginResponseDto(tokenProvider.createToken(authentication));
-        } catch (BadCredentialsException e) {
-            throw new UnauthorizedException();
-        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginDto.getName(),
+                loginDto.getPassword(),
+                List.of(ROLE_OWNER.getAuthority())
+        );
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new OwnerLoginResponseDto(tokenProvider.createToken(authentication));
     }
 }
