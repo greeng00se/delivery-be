eb76eb31a45f8cb08b458449affff55a06b38e5d
greeng00se <thegreengoose7@gmail.com>
2022-10-18 23:17:38 +0900

    [#65] feat: OrderController 추가

---
+++ src/main/java/me/myeats/delivery/order/controller/OrderController.java
@@ -0,0 +1,30 @@
+package me.myeats.delivery.order.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.myeats.delivery.common.jwt.customer.CurrentCustomer;
+import me.myeats.delivery.common.jwt.customer.CustomerPreAuthorized;
+import me.myeats.delivery.customer.domain.Customer;
+import me.myeats.delivery.order.dto.CartDto;
+import me.myeats.delivery.order.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@CustomerPreAuthorized
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/{shopId}")
+    public void order(@RequestBody CartDto cartDto, @PathVariable Long shopId,
+                      @CurrentCustomer Customer customer) {
+        orderService.order(cartDto, shopId, customer.getId());
+    }
+}
