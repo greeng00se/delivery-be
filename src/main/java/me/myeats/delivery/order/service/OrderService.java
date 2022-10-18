522b248b8e6b4132d48482e769b82af5f349cf31
greeng00se <thegreengoose7@gmail.com>
2022-10-18 23:17:23 +0900

    [#65] feat: OrderService 검증 추가

--- src/main/java/me/myeats/delivery/order/service/OrderService.java
+++ src/main/java/me/myeats/delivery/order/service/OrderService.java
@@ -2,10 +2,13 @@ package me.myeats.delivery.order.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.myeats.delivery.common.exception.order.InvalidOrderException;
 import me.myeats.delivery.order.domain.Order;
 import me.myeats.delivery.order.domain.OrderRepository;
 import me.myeats.delivery.order.dto.CartDto;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,7 +18,11 @@ public class OrderService {
     private final OrderMapper orderMapper;
     private final OrderRepository orderRepository;
 
-    public void order(CartDto cartDto) {
+    public void order(CartDto cartDto, Long shopId, Long id) {
+        if (!Objects.equals(cartDto.getShopId(), shopId) ||
+                !Objects.equals(cartDto.getUserId(), id)) {
+            throw new InvalidOrderException();
+        }
         Order order = orderMapper.toOrder(cartDto);
         orderRepository.save(order);
     }
