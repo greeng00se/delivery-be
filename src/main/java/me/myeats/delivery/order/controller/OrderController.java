77c2e9e31bff925bea59526cfb547a6871f02563
greeng00se <thegreengoose7@gmail.com>
2022-10-19 23:32:42 +0900

    [#65] fix: Get -> Post

--- src/main/java/me/myeats/delivery/order/controller/OrderController.java
+++ src/main/java/me/myeats/delivery/order/controller/OrderController.java
@@ -7,8 +7,8 @@ import me.myeats.delivery.common.jwt.customer.CustomerPreAuthorized;
 import me.myeats.delivery.customer.domain.Customer;
 import me.myeats.delivery.order.dto.CartDto;
 import me.myeats.delivery.order.service.OrderService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @GetMapping("/{shopId}")
+    @PostMapping("/{shopId}")
     public void order(@RequestBody CartDto cartDto, @PathVariable Long shopId,
                       @CurrentCustomer Customer customer) {
         orderService.order(cartDto, shopId, customer.getId());
